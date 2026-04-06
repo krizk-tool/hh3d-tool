306f75dc67b98a1e46ea2d1c642b8c14d1323ed1
krizk-tool <thuanhv94@gmail.com>
2026-04-07 06:20:49 +0700

    Update hh3d_krizk.js

--- hh3d_krizk.js
+++ hh3d_krizk.js
@@ -4,8 +4,8 @@
     // @version       5.8.5
     // @description   Custom menu auto HH3D
     // @author        Cre: [Unknown] - Edited by Krizk
-    // @include       *://hoathinh3d.ai*/*
-    // @exclude       *://hoathinh3d.ai/khoang-mach*
+    // @include       *://hoathinh3d.co*/*
+    // @exclude       *://hoathinh3d.co/khoang-mach*
     // @require       https://cdn.jsdelivr.net/npm/sweetalert2@11.26.12/dist/sweetalert2.all.min.js
     // @run-at        document-start
     // @grant         GM_xmlhttpRequest
